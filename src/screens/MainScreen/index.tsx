f8ccabe40d5f3c5ba9bd5fec9303a961261aa834
Angelo <angelo.lucena@ppd.ph>
2021-02-19 14:12:36 +0800

    Convert from class to function component

--- src/screens/MainScreen/index.tsx
+++ src/screens/MainScreen/index.tsx
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { View, ScrollView } from "react-native";
 import { map } from 'lodash';
 import moment from 'moment'
@@ -7,27 +7,68 @@ import moment from 'moment'
 import { TextMedium } from "../../components/TextComponent";
 import { ContainerComponent } from '../../components/ContainerComponent/index';
 import { ImageComponent } from "../../components/ImageComponent";
+import { CardComponent } from '../../components/CardComponent/index';
+import { TextCustomSize, TextExtraLarge } from '../../components/TextComponent/index';
 
 import { getWeatherRequest } from '../../redux/actions/weatherAction';
 
-import { TextCustomSize, TextExtraLarge } from '../../components/TextComponent/index';
 import { styles } from "./styles";
-import { CardComponent } from '../../components/CardComponent/index';
 
-
-interface Props {
-  getWeatherRequest: any;
-  loading: boolean;
-  weather: any;
-  error: any;
+function getWeatherIcon(iconName: string) {
+  return `http://openweathermap.org/img/wn/${iconName}@4x.png`;
 }
 
-class MainScreen extends Component<Props> {
-  componentDidMount() {
-    this.getWeather();
-  }
+function renderWeatherDetails(weather: any) {
+  const weatherItems = [
+    {
+      label: 'Description',
+      value: weather.weather[0].description
+    },
+    {
+      label: 'Feels like',
+      value: `${weather.main.feels_like} °C`
+    },
+    {
+      label: 'Humidity',
+      value: `${weather.main.humidity} %`
+    },
+    {
+      label: 'Pressure',
+      value: `${weather.main.pressure} hPa`
+    },
+    {
+      label: 'Clouds',
+      value: `${weather.clouds.all} %`
+    },
+    {
+      label: 'Wind',
+      value: `${weather.wind.speed} meter/sec`
+    },
+    {
+      label: 'Sunrise',
+      value: moment.unix(weather.sys.sunrise).format('h:mm a')
+    },
+    {
+      label: 'Sunset',
+      value: moment.unix(weather.sys.sunset).format('h:mm a')
+    }
+  ];
+  return (
+    map(weatherItems, (items, i) =>
+      <CardComponent key={i} style={styles.weatherDetailItems}>
+        <TextMedium bold={true}>{`${items.label}: `}</TextMedium>
+        <TextMedium>{items.value}</TextMedium>
+      </CardComponent>
+    )
+  )
+}
 
-  getWeather() {
+
+export default function MainScreen() {
+  const dispatch = useDispatch();
+  const state = useSelector(state => state.weatherReducer)
+
+  useEffect(() => {
     // getCurrentLocation().then((location: any) => {
     //     this.props.getWeatherRequest({
     //         lat: location.latitude,
@@ -35,102 +76,37 @@ class MainScreen extends Component<Props> {
     //         units: 'metric'
     //     });
     // });
-    this.props.getWeatherRequest({
+    dispatch(getWeatherRequest({
       q: 'Pasig',
       units: 'metric'
-    });
-  }
-
-  getWeatherIcon = (iconName: string) => {
-    return `http://openweathermap.org/img/wn/${iconName}@4x.png`;
-  }
-
-  render() {
-    const { loading, weather, error } = this.props;
-    return (
-      <ContainerComponent>
-        {
-          weather ?
-            <View style={styles.container}>
-              <CardComponent style={styles.weatherMainContainer} >
-                <View style={styles.rowContainer}>
-                  <View>
-                    <View style={styles.weatherIconContainer}>
-                      <ImageComponent source={{ uri: this.getWeatherIcon(weather.weather[0].icon) }} />
-                    </View>
-                  </View>
-                  <View style={styles.weatherMainDataContainer}>
-                    <TextExtraLarge testID={'weatherMain'} bold={true}>{weather.weather[0].main}</TextExtraLarge>
-                    <TextCustomSize bold={true} fontSize={40}>{`${weather.main.temp} °C`}</TextCustomSize>
-                    <TextMedium>{`${weather.main.temp_max} °C | ${weather.main.temp_min} °C`}</TextMedium>
+    }))
+  }, [state.weather]);
+  return (
+    <ContainerComponent>
+      {
+        state.weather ?
+          <View style={styles.container}>
+            <CardComponent style={styles.weatherMainContainer} >
+              <View style={styles.rowContainer}>
+                <View>
+                  <View style={styles.weatherIconContainer}>
+                    <ImageComponent source={{ uri: getWeatherIcon(state.weather.weather[0].icon) }} />
                   </View>
                 </View>
-                <TextMedium style={styles.date}>{moment().format('MMMM D, h:mm a')}</TextMedium>
-              </CardComponent>
-              <View>
-                <ScrollView>{this.renderWeatherDetails(weather)}</ScrollView>
+                <View style={styles.weatherMainDataContainer}>
+                  <TextExtraLarge testID={'weatherMain'} bold={true}>{state.weather.weather[0].main}</TextExtraLarge>
+                  <TextCustomSize bold={true} fontSize={40}>{`${state.weather.main.temp} °C`}</TextCustomSize>
+                  <TextMedium>{`${state.weather.main.temp_max} °C | ${state.weather.main.temp_min} °C`}</TextMedium>
+                </View>
               </View>
-            </View >
-            : null
-        }
-      </ContainerComponent >
-    );
-  }
-
-  renderWeatherDetails(weather: any) {
-    const weatherItems = [
-      {
-        label: 'Description',
-        value: weather.weather[0].description
-      },
-      {
-        label: 'Feels like',
-        value: `${weather.main.feels_like} °C`
-      },
-      {
-        label: 'Humidity',
-        value: `${weather.main.humidity} %`
-      },
-      {
-        label: 'Pressure',
-        value: `${weather.main.pressure} hPa`
-      },
-      {
-        label: 'Clouds',
-        value: `${weather.clouds.all} %`
-      },
-      {
-        label: 'Wind',
-        value: `${weather.wind.speed} meter/sec`
-      },
-      {
-        label: 'Sunrise',
-        value: moment.unix(weather.sys.sunrise).format('h:mm a')
-      },
-      {
-        label: 'Sunset',
-        value: moment.unix(weather.sys.sunset).format('h:mm a')
+              <TextMedium style={styles.date}>{moment().format('MMMM D, h:mm a')}</TextMedium>
+            </CardComponent>
+            <View>
+              <ScrollView>{renderWeatherDetails(state.weather)}</ScrollView>
+            </View>
+          </View >
+          : null
       }
-    ];
-    return (
-      map(weatherItems, (items, i) =>
-        <CardComponent key={i} style={styles.weatherDetailItems}>
-          <TextMedium bold={true}>{`${items.label}: `}</TextMedium>
-          <TextMedium>{items.value}</TextMedium>
-        </CardComponent>
-      )
-    )
-  }
+    </ContainerComponent >
+  )
 }
-
-const mapStateToProps = (state: any) => ({
-  loading: state.weatherReducer.loading,
-  weather: state.weatherReducer.weather,
-  error: state.weatherReducer.error
-})
-
-const mapDispatchToProps = (dispatch: any) => ({
-  getWeatherRequest: (params: any) => dispatch(getWeatherRequest(params))
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(MainScreen);
